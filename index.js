5c56a0c3ac244b6ad1eb81db5b681466983045a4
AntLafI <antoine.lafitte@etu.univ-amu.fr>
2023-04-03 09:43:44 +0200

    ça marche vraiment pour android et ios mtn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-Platform.OS === 'ios' ? AppRegistry.registerComponent(appName, () => App) : AppRegistry.registerComponent('main', () => App);
+Platform.OS === 'ios' ? AppRegistry.registerComponent('main', () => App) : AppRegistry.registerComponent(appName, () => App);
 
