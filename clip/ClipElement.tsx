05c4a573f86c2328c78672479603929adb67adc7
Romain CLEMENT <rc2301@orange.fr>
2023-04-03 11:34:42 +0200

    Copied title to clipboard

--- clip/ClipElement.tsx
+++ clip/ClipElement.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Clipboard } from 'react-native';
 import IconVector from 'react-native-vector-icons/FontAwesome5';
 
 export default class ClipElement extends React.Component<any, any> {
@@ -11,7 +11,7 @@ export default class ClipElement extends React.Component<any, any> {
     render(): JSX.Element {
         return <View style={{flex:1,margin:10,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
             <Text style={{fontSize:20,}}>{this.props.title}</Text>
-            <IconVector name="clipboard" size={40} />
+            <IconVector name="clipboard" size={40} onPress={() => Clipboard.setString(this.props.title)} />
         </View>;
     }
 }
