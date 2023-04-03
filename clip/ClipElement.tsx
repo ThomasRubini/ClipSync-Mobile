4e34f2ce373be051c0e2a86af5d037528d4499eb
Romain CLEMENT <rc2301@orange.fr>
2023-04-03 11:51:01 +0200

    Added toast on copy

--- clip/ClipElement.tsx
+++ clip/ClipElement.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Clipboard } from 'react-native';
 import IconVector from 'react-native-vector-icons/FontAwesome5';
+import Toast from 'react-native-simple-toast';
 
 export default class ClipElement extends React.Component<any, any> {
 
@@ -8,10 +9,15 @@ export default class ClipElement extends React.Component<any, any> {
         super(props);
     }
 
+    onCopy() {
+        Clipboard.setString(this.props.title);
+        Toast.show('Put "' + this.props.title + '" in clipboard', Toast.SHORT);
+    }
+
     render(): JSX.Element {
         return <View style={{flex:1,margin:10,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
             <Text style={{fontSize:20,}}>{this.props.title}</Text>
-            <IconVector name="clipboard" size={40} onPress={() => Clipboard.setString(this.props.title)} />
+            <IconVector name="clipboard" size={40} onPress={() => this.onCopy()} />
         </View>;
     }
 }
