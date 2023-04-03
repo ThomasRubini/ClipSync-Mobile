7db1a7e3d3a845d8fc0e4a02c4e445a6acbbffa8
Romain CLEMENT <rc2301@orange.fr>
2023-04-03 10:57:34 +0200

    Added Copy button

--- clip/ClipElement.tsx
+++ clip/ClipElement.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import IconVector from 'react-native-vector-icons/FontAwesome5';
 
 export default class ClipElement extends React.Component<any, any> {
 
@@ -8,8 +9,9 @@ export default class ClipElement extends React.Component<any, any> {
     }
 
     render(): JSX.Element {
-        return <View>
-            <Text>{this.props.title}</Text>
+        return <View style={{flex:1,margin:10,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+            <Text style={{fontSize:20,}}>{this.props.title}</Text>
+            <IconVector name="clipboard" size={40} />
         </View>;
     }
 }
